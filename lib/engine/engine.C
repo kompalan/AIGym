1d4d9460a9bfb7e2af54202db2aacdba371257b0
kompalan <kompalli@msu.edu>
2023-04-18 11:54:24 -0400

    Added initial files and build system

---
+++ lib/engine/engine.C
@@ -0,0 +1,8 @@
+#include "engine.H"
+
+using Engine::AnuragEngine;
+
+void AnuragEngine::DeliverMessage()
+{
+    std::cout << this->message << std::endl;
+}
---
+++ src/main.C
@@ -0,0 +1,7 @@
+#include "engine.H"
+
+int main() {
+    Engine::AnuragEngine anurag;
+    anurag.DeliverMessage();
+    return 0;
+}
