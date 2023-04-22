936b4c47e39e9669d6efe15cf5a6cbe1eaee5362
Anurag Kompalli <kompalli@msu.edu>
2023-04-21 20:28:06 -0400

    Added Vulkan support and example code to open a blank window
    Added support for spdlog for logging, vulkan for graphics and glfw to
    manage the windows. Additionally, updated the .gitignore to ignore .idea
    files from CLion. spdlog and glfw are added as submodules.

--- src/main.C
+++ src/main.C
@@ -1,7 +1,31 @@
+#include "spdlog/spdlog.h"
+
+#define GLFW_INCLUDE_VULKAN
+#include <vulkan/vulkan.h>
+#include <GLFW/glfw3.h>
+
 #include "engine.H"
 
+
 int main() {
-    Engine::AnuragEngine anurag;
-    anurag.DeliverMessage();
+    glfwInit();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+
+    spdlog::info("Extensions Supported: {}", extensionCount);
+
+    while(!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+
     return 0;
 }
